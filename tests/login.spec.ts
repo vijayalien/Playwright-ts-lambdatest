83398b77d9ed9600dfaf57d6624585246c650fdf
vijay <vijaysalian94@gmail.com>
2023-09-26 22:30:39 +1000

    first commit

---
+++ tests/login.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect, chromium } from '@playwright/test';
+
+
+test("login test demo", async () => {
+const browser = await chromium.launch({headless:false});
+const context = await browser.newContext();
+const page = await context.newPage();
+
+await page.goto("https://ecommerce-playground.lambdatest.io/");
+await page.hover("//a[@data-toggle='dropdown']//span[contains(.,'My account')]");
+await page.click("'Login'")
+
+await page.fill("input[name='email']", "koushik350@gmail.com")
+await page.fill("input[name='password']", "Pass123$")
+await page.click("input[value='Login']");
+
+await page.close();
+await context.close();
+await browser.close();
+
+})
