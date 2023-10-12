f92dba3676bdcfd282d23f8516bb26069205024b
vijay <vijaysalian94@gmail.com>
2023-10-12 23:04:53 +1100

    minor changes

--- pages/specialHotPage.ts
+++ pages/specialHotPage.ts
@@ -1,21 +1,21 @@
 import { Page } from "@playwright/test";
 
-export default class SpecialHotPage{
+export default class SpecialHotPage {
 
 
     constructor(public page: Page) { }
 
-    async addFirstProductToTheCart() { 
+    async addFirstProductToTheCart() {
         await this.page.hover("//div[@class='image']/a", {
             strict: false
         });
         await this.page.locator("(//button[@title='Add to Cart'])")
             .nth(0).click();
     }
-    async isToastVisible() { 
+    async isToastVisible() {
         // await this.page.waitFor
         const toast = this.page.locator("//a[.='View Cart ']");
-        await toast.waitFor({state:"visible"})
+        await toast.waitFor({ state: "visible" })
         return toast;
     }
 }
